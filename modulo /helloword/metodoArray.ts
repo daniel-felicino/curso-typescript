a0e78be25bcdbe6e75916d82a4b1eaab6bd2a68a
https://github.com/daniel-felicino <daniel-feliciano@hotmail.com>
2023-02-25 17:02:29 -0300

    // console.log(animais.splice(2,2))

---
+++ modulo /helloword/metodoArray.ts
@@ -0,0 +1,43 @@
+type array = {
+
+  nome: string,
+  sobrenome: string,
+  cidade: string,
+}
+
+function acessoArray(paremetro: array) {
+
+  for (var  i = 0; i < acessoArray.length; i++) {
+
+    console.log(`Os paramentros são : ${paremetro} `)
+  }
+}
+acessoArray({nome:'Daniel',sobrenome:"Silva",cidade:'Uberaba'})
+
+
+ var animais:string[] = ['abelha','cão','gato','elefante','baleia']
+// var insetos:string[] = ['pernelongo','barata','mosca','formiga','aranha']
+
+// animais.pop()
+// console.log()
+// animais.push('leão')
+// console.log(animais)
+// animais.sort()
+// console.log(animais)
+//  var instos = ['pernelongo','barata','mosca','formiga','aranha']
+// var e = animais.concat(instos)
+//  console.log(e)
+
+// console.log(animais.shift())
+
+// console.log(animais.splice(0,1))
+// console.log(animais.splice(1,0,'daniel'))
+// console.log(animais.splice(2,2))
+animais.unshift('daniel')
+console.log(animais)
+
+// console.log(animais.slice(1,4))
+
+// console.log(animais.toString())
+
+
--- modulo /helloword/tempCodeRunnerFile.js
+++ modulo /helloword/tempCodeRunnerFile.js
@@ -1,17 +1,2 @@
 "use strict";
-class racasdecao {
-    constructor(parametroNome, parametroIdade, paramentroRaca) {
-        this.nome = parametroNome;
-        this.idade = parametroIdade;
-        this.racas = paramentroRaca;
-        racasdecao.vendas++;
-        console.log(racasdecao.vendas);
-    }
-    dadosdoanimal() {
-        console.log(`Dados do Animial Nome: ${this.nome} Idade: ${this.idade}`);
-    }
-}
-racasdecao.vendas = 0;
-const caovendido = new racasdecao('Duda', 4, [' budog']);
-const caovendido2 = new racasdecao('Raf', 5, ['vira lata']);
-console.log(caovendido.dadosdoanimal());
+resultadqo;
